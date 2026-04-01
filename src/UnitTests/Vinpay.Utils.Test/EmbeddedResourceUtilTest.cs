9b59a7fc3c1c3fb74be24fde7d989298ed237bbc
drawinpay <drawinpay@qq.com>
2026-04-01 11:13:42 +0800

    Complete EmbeddedResourceUtilTest.

--- src/UnitTests/Vinpay.Utils.Test/EmbeddedResourceUtilTest.cs
+++ src/UnitTests/Vinpay.Utils.Test/EmbeddedResourceUtilTest.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Vinpay.Utils.Reflection;
 
 namespace Vinpay.Utils.Test;
@@ -6,45 +7,98 @@ namespace Vinpay.Utils.Test;
 public class EmbeddedResourceUtilTest
 {
     private const string EmbeddedResourcePath = "Resources.EmbeddedResourceTest.text1.txt";
+    private const string EmbeddedResourceContent = "123ABC+-*/";
+
+    private static Assembly TestAssembly => typeof(EmbeddedResourceUtilTest).Assembly;
 
     [TestMethod]
     public void GetEmbeddedText_WithEmbeddedResource_ReturnsExpectedText()
     {
-        var assembly = typeof(EmbeddedResourceUtilTest).Assembly;
+        string result = EmbeddedResourceUtil.GetEmbeddedText(TestAssembly, EmbeddedResourcePath);
 
-        string result = EmbeddedResourceUtil.GetEmbeddedText(assembly, EmbeddedResourcePath);
-
-        Assert.AreEqual("123ABC+-*/", result);
+        Assert.AreEqual(EmbeddedResourceContent, result);
     }
 
     [TestMethod]
+    public void GetEmbeddedText_WithNullEncoding_UsesUtf8ByDefault()
+    {
+        string result = EmbeddedResourceUtil.GetEmbeddedText(TestAssembly, EmbeddedResourcePath, null);
+
+        Assert.AreEqual(EmbeddedResourceContent, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FileNotFoundException))]
     public void GetEmbeddedText_WithMissingResource_ThrowsFileNotFoundException()
     {
-        var assembly = typeof(EmbeddedResourceUtilTest).Assembly;
+        _ = EmbeddedResourceUtil.GetEmbeddedText(TestAssembly, "Resources.EmbeddedResourceTest.not-found.txt");
+    }
 
-        Assert.ThrowsException<FileNotFoundException>(() =>
-            EmbeddedResourceUtil.GetEmbeddedText(assembly, "Resources.EmbeddedResourceTest.not-found.txt"));
+    [TestMethod]
+    [ExpectedException(typeof(FileNotFoundException))]
+    public void GetEmbeddedText_WithEmptyRelativePath_ThrowsFileNotFoundException()
+    {
+        _ = EmbeddedResourceUtil.GetEmbeddedText(TestAssembly, string.Empty);
     }
 
     [TestMethod]
     public void OutputEmbeddedResource_WithEmbeddedResource_WritesExpectedFile()
     {
-        var assembly = typeof(EmbeddedResourceUtilTest).Assembly;
         string outputPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.txt");
 
         try
         {
-            EmbeddedResourceUtil.OutputEmbeddedResource(assembly, EmbeddedResourcePath, outputPath);
+            EmbeddedResourceUtil.OutputEmbeddedResource(TestAssembly, EmbeddedResourcePath, outputPath);
 
             Assert.IsTrue(File.Exists(outputPath));
-            Assert.AreEqual("123ABC+-*/", File.ReadAllText(outputPath));
+            Assert.AreEqual(EmbeddedResourceContent, File.ReadAllText(outputPath));
         }
         finally
         {
-            if (File.Exists(outputPath))
-            {
-                File.Delete(outputPath);
-            }
+            DeleteFileIfExists(outputPath);
+        }
+    }
+
+    [TestMethod]
+    public void OutputEmbeddedResource_WithExistingFile_OverwritesContent()
+    {
+        string outputPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.txt");
+
+        try
+        {
+            File.WriteAllText(outputPath, "old-content");
+
+            EmbeddedResourceUtil.OutputEmbeddedResource(TestAssembly, EmbeddedResourcePath, outputPath);
+
+            Assert.AreEqual(EmbeddedResourceContent, File.ReadAllText(outputPath));
+        }
+        finally
+        {
+            DeleteFileIfExists(outputPath);
+        }
+    }
+
+    [TestMethod]
+    public void OutputEmbeddedResource_WithMissingResource_ThrowsNullReferenceException()
+    {
+        string outputPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.txt");
+
+        try
+        {
+            Assert.ThrowsException<NullReferenceException>(() =>
+                EmbeddedResourceUtil.OutputEmbeddedResource(TestAssembly, "Resources.EmbeddedResourceTest.not-found.txt", outputPath));
+        }
+        finally
+        {
+            DeleteFileIfExists(outputPath);
+        }
+    }
+
+    private static void DeleteFileIfExists(string path)
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
         }
     }
 }
